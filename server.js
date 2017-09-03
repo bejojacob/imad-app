cb71b847214aca6970c11cab4f8151a6f62bc6a6
bejojacob <31561810+bejojacob@users.noreply.github.com>
2017-09-03 16:29:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
-
 app.get ('/article one', function (req, res ){
     res.send('article one request and will be served here');
      
